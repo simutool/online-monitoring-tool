943ebf79ba88a0202ea334b34670adf41bfb65a4
Valentyna Voronova <valentyna.voronova@stud.uni-bamberg.de>
2018-11-25 02:47:22 +0100

    comments collapsing

--- src/github.com/grafana/grafana/public/app/custom.ts
+++ src/github.com/grafana/grafana/public/app/custom.ts
@@ -1,8 +1,8 @@
 import $ from 'jquery';
- import  '../../node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min';
  import 'moment';
     declare var $: $;
 
+	const springHost = 'http://localhost:8090';
 
 
  $(window).on('load',function(){
@@ -12,38 +12,52 @@ import $ from 'jquery';
 	$(window).on('load',function(){
         $.ajax({
 			method: 'GET',
-			url: 'http://localhost:8090/getExperimentData',
+			url: springHost + '/getExperimentData',
 			headers: {
 				'Access-Control-Allow-Origin': '*'
 		},
 		}).then(function successCallback(response) {
 			console.log(response);
+			
 			var counter = 0;
 			for(let panel of response){
+				//Set panel names
 				$($(".panel-title-text")[counter]).text(panel.name);
-				console.log(panel.name);
-			
-			
-			console.log($(".customTimepicker")[counter]);
 
-			$($(".customTimepicker")[counter]).val(new Date().toLocaleTimeString());
+				//Set initial time in time input
+				$($(".customTimepicker")[counter]).val(new Date().toLocaleTimeString());
 
+				//Set current time when time input is in focus
+				$($(".customTimepicker")[counter]).on("focus", function(event){
+					this.value = new Date().toLocaleTimeString();
+				});
 			
-			$($(".customTimepicker")[counter]).on("focus", function(event){
-				this.value = new Date().toLocaleTimeString();
-				console.log(event.type);
-            });
-			
-				
-			$($(".commentBody")[counter]).on("focus", setCustomTime.bind(this, counter));
-			
-			function setCustomTime(counter){
+				//Set current time every time comment body is in focus
+				$($(".commentBody")[counter]).on("focus", setCustomTime.bind(this, counter));	
+				function setCustomTime(counter){
 						$($(".customTimepicker")[counter]).val(new Date().toLocaleTimeString());
-				console.log(counter);
-				console.log("comment body on focus");
-			}
-			
-			counter++;
+				}
+								console.log($($(".commentsCont")[counter]));
+
+				//Connect comment sections with buttons that toggle them
+				$($(".commentsCont")[counter]).attr("id", "commentsCont" + counter);
+				$($(".commentSectionToggle")[counter]).attr("aria-controls", "commentsCont" + counter);
+				$($(".commentSectionToggle")[counter]).attr("data-target", "#commentsCont" + counter);
+				
+				$($(".commentSectionToggle")[counter]).on("click", toggleCommentBox.bind(this, counter));
+				function toggleCommentBox(counter){
+					
+						var cont = $($(".commentsCont")[counter]);
+						console.log(cont.hasClass("hidden"));
+						if(cont.hasClass("hidden")){
+							cont.removeClass("hidden");
+						}else{
+							cont.addClass("hidden");
+						}
+				}
+
+
+				counter++;
 			}	
 		}, function errorCallback(response) {
 				console.log(response);
--- src/github.com/grafana/grafana/public/app/plugins/panel/graph/template.ts
+++ src/github.com/grafana/grafana/public/app/plugins/panel/graph/template.ts
@@ -1,14 +1,25 @@
 const template = `
-<div class="graph-panel" ng-class="{'graph-panel--legend-right': ctrl.panel.legend.rightSide}">
+<div class="graph-panel" ng-class="{'graph-panel--legend-right': ctrl.panel.legend.rightSide}" >
   <div class="graph-panel__chart" grafana-graph ng-dblclick="ctrl.zoomOut()">
   </div>
 
   <div class="graph-legend">
     <div class="graph-legend-content" graph-legend></div>
   </div>
+  
+  <div class="row panelButtons">
+	<a class="btn navbar-button  commentSectionToggle"  role="button" >
+			Comments
+	</a>
+	<a class="btn navbar-button "  role="button" >
+			Save as CSV
+	</a>
+  </div>
+  
+	<div class="row commentsCont collapse" >
 	
-	<div class="row commentsCont" style="height: 300px">
-		<div class="col-sm-12 text-center">
+		
+		<div class="col-sm-12 text-center commentsToHide">
 			<div class="input-group">
 				<textarea  type="text"  rows="3" class="col-xs-12 form-control input-sm chat-input commentBody" placeholder="Write your message here..." ></textarea>
 				<div class="row">
@@ -32,19 +43,22 @@ const template = `
 				</div>
 			</div>
 			<hr data-brackets-id="12673">
-			<ul data-brackets-id="12674" id="sortable" class="list-unstyled ui-sortable">
-				<strong class="pull-left primary-font">James</strong>
-				<small class="pull-right text-muted">
-				<span class="glyphicon glyphicon-time"></span>7 mins ago</small>
-				</br>
-				<li class="ui-state-default">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
-				</br>
-				<strong class="pull-left primary-font">Taylor</strong>
-				<small class="pull-right text-muted">
-				<span class="glyphicon glyphicon-time"></span>14 mins ago</small>
-				</br>
-				<li class="ui-state-default">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-        
+			<ul data-brackets-id="12674" id="sortable" class="list-unstyled ui-sortable" style="height: 100px;overflow: auto">
+				<div>
+					<strong class="pull-left primary-font">James</strong>
+					<small class="pull-right text-muted">
+					<span class="glyphicon glyphicon-time"></span>7 mins ago</small>
+					</br>
+					<li class="ui-state-default">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
+					</br>
+				</div>
+				<div>
+					<strong class="pull-left primary-font">Taylor</strong>
+					<small class="pull-right text-muted">
+					<span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+					</br>
+					<li class="ui-state-default">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+				</div>
 			</ul>
 		</div>
 	</div>
